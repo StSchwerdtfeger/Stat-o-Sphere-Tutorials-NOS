ee51abfbf05e913a08abea55dc4f971506d1c559
Steffen Schwerdtfeger <88867126+StSchwerdtfeger@users.noreply.github.com>
2024-04-28 18:54:09 +0200

    Update and rename RBasic_CIPom.R to RBasic_CIPom_I.R

--- R-Basics@CIPom/RBasic_CIPom_I.R
+++ R-Basics@CIPom/RBasic_CIPom_I.R
@@ -1,6 +1,6 @@
 ##################################
 ##################################
-#        R-Basic Tutorial        #
+#       R-Basic Tutorial I       #
 #     CIPom @ Charité Edition    #
 #               by               #
 #     Steffen Schwerdtfeger      # 
@@ -13,8 +13,10 @@
 ############################
 
 # Mark line and execute via ALT+ENTER or Cmnd+ENTER (Mac)
-test = 2 + 5
+test = 2 + 5 
 test = 2 + 3
+
+
 
 ###################################################################
 # 4 Classes of Objects: Vectors, Matrices, Arrays, Lists and More #
@@ -24,13 +26,15 @@ vec = c(1,2,3)
 # [1] 1 2 3  # NOTE that an output is also a vector
 #      therefore the [1] at the beginning.
 
-vec[2]  # index 2 == second element 
+vec[3]  # index 2 == second element 
 # [1] 2 # second element in the vector is also 2
+
 
 # Typical error:
 # > # Typical Error:
 # > vec = c(
 # + 
+
 
 # SINCE IT SAYS "+" it means that the operation is not finished yet, since
 # the second ")" is missing. In fact, when selecting the whole script, R
@@ -38,6 +42,7 @@ vec[2]  # index 2 == second element
 # brackets. IN ANY CASE you have type in the missing ")" and execute that line again.
 # It will then show an error. We also recommend using the brushtool in the environment!
 
+class(vec)
 mat = as.matrix(vec)
 #      [,1]
 # [1,]    1
@@ -142,7 +147,6 @@ as.numeric(test)
 # NAs introduced by coercion
 
 
-
 ########################
 # 5 Data Cleaning in R #
 ########################
@@ -154,11 +158,12 @@ fam = c("yes", "yes", "no", "no", NA, NA, "n","n","no","no","ys", "ys", NA, NA)
 time = c("t1","t2","t1","t2","t1","t2","t1","t2","t1","t2","t1","t2","t1","t2")
 measurement_sysRR = c(130,122,132,123,133,121,129,125,135,119,134,127,140,125)
 
+
 # Format into a data frame:
 table = as.data.frame(cbind(patient_id,time,measurement_sysRR,fam))
 
 # install.packages("dplyr")  # install package
-library(dplyr)             # load/activate package
+library(dplyr)               # load/activate package
 # Filter function: filter(data_object, columnname == "entry") or != for unequal
 t1 = filter(table, time == "t1")
 t1 = filter(table, time != "t2") # alternative
@@ -203,6 +208,11 @@ for(i in 1:length(na_lines)){
 pat_id_na
 #[1] "2" "4" # Patient ids with NA in either t1 or t2
 
+# Analoge:
+new_table$patient_id[4]
+new_table$patient_id[7]
+
+
 # Initilize table:
 fin_table = new_table
 # Delete every line with the patient_id given in the list
@@ -211,6 +221,26 @@ for(i in 1:length(pat_id_na)){ # or length(na_lines)
   fin_table = filter(fin_table, patient_id != pat_id_na[i])
 } # End for i
 fin_table 
+
+## Example For loop:
+
+# Define an object you want to loop over:
+object = c(1,1,1,1)
+
+# Initilize a list to store the results:
+list_results = c()
+
+# For loop that adds +1 to every element of the above object:
+for(i in 1:length(object)){
+  list_results[i] = object[i]+1
+} # End for i
+list_results
+
+# Analog: 
+list_results[1] = object[1]+1
+list_results[2] = object[2]+1
+list_results[3] = object[3]+1
+list_results[4] = object[4]+1
 
 
 #### POSSIBLE SOLUTION II:
@@ -252,6 +282,7 @@ fin_table$patient_id == fin_table_alt$patient_id
 
 
 ### POSSIBLE SOLUTION II Simple dplyr/tidyverse alternative:
+new_table_alt = filter(new_table, is.na(measurement_sysRRalt) == FALSE)
 new_table_alt %>%   # new_table_alt was filtered for NAs already 
   select(patient_id,time,measurement_sysRRalt)%>%
   group_by(patient_id) %>%  # sorts individ. entries into indiv. groups 
@@ -271,9 +302,9 @@ library("tidyverse")
 # Test 1 Filter von t2 und Zeilen mit NA löschen
 test1 = new_table %>% 
   select(patient_id,time,measurement_sysRRalt) %>% # select columns like cbind()
-  filter(time != "t1")  # Filter for t1
-  # alternative: 
-  # na.omit() # be careful with this function
+  filter(time != "t1")%>%  # Filter for t1
+  na.omit()
+  # na.omit() # be careful with this function!!!
               # and no %>% at the end, otherwise "+" in console!
 
 test1
@@ -285,14 +316,6 @@ test1
 # 6          6   t2                  127
 # 7          7   t2                  125
 
-test2 = new_table %>% 
-  select(patient_id,time,measurement_sysRRalt) %>% # select columns like cbind()
-  filter(time != "t1") %>% # Filter for t1
-  summarize(mean_t1 = mean(measurement_sysRR)) # Way to organize output
-
-#    mean_t1
-# 1 128.2143  # The "1" at the beginning is not [1] since the output
-              # is a table/tibble in this case.
 
 # Example na.omit()
 test_na_omit = cbind(c(1,NA,3,4), c(1,2,NA,4))
@@ -428,229 +451,6 @@ sum_alt(c(1,2,3))
 # Test for equal results:
 sum(c(1,2,3)) == sum_alt(c(1,2,3))
 # [1] TRUE
-
-
-##################################################################################
-# 8 Einstieg in die Inferenzstatistik: Bedingte Wahrscheinlichkeit / Bayes‘ Rule #
-##################################################################################
-
-###### Benfords law example, number between 0 and 10: 
-n=seq(0,10,by=.1)
-benford = log(n+1)-log(n)
-plot(n,benford)
-
-###### Numeric example cond. probability / Bayes' rule:
-prior = c(.5,.5)       # Can be set by us!
-likelihood = c(.6,.4)  # Can be set by us!
-posterior = (prior*likelihood)/sum(prior*likelihood)
-# [1] 0.6 0.4
-
-###### Vanessa Holmes example:
-
-# First iteration:
-joint = c(.25,.25,.25,25)*c(.33,.33,.33,0)
-model_evidence = sum(c(.25,.25,.25,.25)*c(.33,.33,.33,0))
-posterior = joint/model_evidence
-posterior
-
-# Second iteration (another suspect ruled out):
-joint = c(.33,.33,.33,0)*c(.5,.5,0,0)
-model_evidence = sum(c(.33,.33,.33,0)*c(.5,.5,0,0))
-posterior = joint/model_evidence
-posterior
-
-# Third iteration - let's say all of the suspects are ruled out:
-joint = c(.5,.5,0,0)*c(0,0,0,0)
-# [1] 0 0 0 0
-model_evidence = sum(c(.5,.5,0,0)*c(0,0,0,0))
-# [1] 0 
-posterior = joint/model_evidence # Note that it says: 0 divided by 0!
-posterior
-
-# [1] NaN NaN NaN NaN 
-# NaN = Not a number.
-# This is due to the fact that 0/0 is not defined. At this point
-# Vanessa Holmes would need to find another suspect to be able 
-# to do further inference...
-
-
-
-#####################################################################################
-# 9 Lineare Regression via Methode der kl. Fehlerquadrate + via deskript. Parameter # 
-#####################################################################################
-
-cups = c(0:10)
-time = c(0:10)
-plot(x=time, y=cups)
-
-# lm(y~x) => y under the condition of x
-lm(cups~time)
-# Add linear model to plot above
-abline(lm(cups~time))
-
-# Errors/residuals:
-# error = y-f(x); f(x) = x
-error = cups - time
-# [1] 0 0 0 0 0 0 0 0 0 0 0
-error_alt = residuals(lm(cups~time))
-#             1             2             3             4             5             6             7 
-# -2.766552e-16  6.000627e-16  2.767107e-17 -1.331760e-16 -7.197851e-17 -1.218033e-16 -1.716281e-16 
-#             8             9            10            11 
-#  2.226363e-16 -2.712777e-16 -3.211025e-16  5.172512e-16 
-
-# Are both approaches equal (enough)?
-all.equal(error,as.numeric(error_alt))
-
-# Let us have a look at our dino:
-plot(x= dino$x, y = dino$y)
-abline(lm(dino$y~dino$x))
-
-### Non idealized Lady Meow Example:
-# Cups of Tea:
-cups = c(0, 1, 2, 3.5, 4.8, 5.2, 6, 6.9, 8.5, 9.1, 9.9,
-         0, .6, 2.6, 3.1, 4.8, 6.9, 7.1, 7.5, 8.5, 9.9, 9.9,
-         0, .2, 2.9, 2.9, 4.9, 5.2, 6.9, 7.1, 7.3, 9, 9.8)
-
-# Time passed for each run of measurements (cups), abbreviated:
-time = c(0:10,0:10,0:10)
-
-
-# You can add labes to the plot!
-plot(x=time, y=cups,
-     ylab = "Cups of Tea", xlab= "Hours passed")
-
-
-#### Lin. Reg. via deskript. Parameters:
-
-x = time
-y = cups
-
-# SumSqXY = ∑(x_i - E[X])*(y_i - E[y])
-SumSqXY = sum((x-mean(x))*(y-mean(y)))
-
-# Covariance:
-covarXYsamp = SumSqXY / (length(x)-1) # sample covariance
-# [1] 10.39687
-covarXYpop  = SumSqXY / length(x)     # population covariance
-# [1] 10.08182
-covarXYsamp = cov(x,y)                # samp cov := cov(x,y)
-# [1] 10.39687
-
-
-##############################################################################
-# 10 Z- und T-Test für einfache Verteilung und T-Test für Lineare Regression #
-##############################################################################
-
-# Our goal:
-summary(lm(cups~time))
-
-# Density via density() function:
-plot(density(time))
-
-
-#### Simulation Central Limit Theorem:
-
-# Initialize data via runif, which will
-# give us a random but uniformly distributed
-# set of data. Each time you run this line, the result will
-# be another set of data points.
-# Uses set.seed(0) or other values to make results reproducible
-# (we will not get further into the the topic here):
-data = runif(n = 1000, min = 1, max = 10)
-
-# We can also plot our data as histogram
-hist(data, col = "lightblue")
-
-# Use a for loop to take 5000 random samples of size n = 5 each:
-n_samp = 5000
-sample_n_5 = c() # create empty vector
-
-# The below function will also take the mean of the sample and 
-# will store the output in our vector sample_n_5:
-for (i in 1:n_samp){
-  # Store mean of each sample with length 5:
-  sample_n_5[i] = mean(sample(data, 5, replace=TRUE))
-} # End for i
-
-# Look at the histogram of the sample_n_5:
-hist(sample_n_5, col = "lightblue")
-
-
-### T-Test:
-
-# Basic R-function:
-x = c(0:10)
-y = c(0:10)*3
-t.test(x,y) # Welch test was automatically chosen (t-test for unequal variance)
-
-
-# PDF of a T-distribution and stand. normal PDF, 
-# comparing different df:
-seq = seq(-4,4,by=.1)
-plot(seq,dnorm(seq, mean = 0, sd = 1), type = "l")
-lines(seq,dt(seq, df = 1), type = "l", col = "green")
-lines(seq,dt(seq, df = 3), type = "l", col = "blue")
-lines(seq,dt(seq, df = 10), type = "l", col = "red")
-
-### Effect size Cohen's d:
-# install.packages("effsize")
-library(effsize)
-x = c(0:10)
-y = c(0:10)+3.5
-cohen.d(x, y) 
-# Here a difference of -1.05529
-cohens_d_formular = (mean(x)-mean(y))/sd(y)
-
-# Here you can see how 
-plot(x= seq(-7,25, by=.1), y = dnorm(seq(-7,25, by=.1),mean(x), sd = sd(x)), type = "l")
-lines(x = seq(-10,35, by=.1), y = dnorm(seq(-10,35, by=.1),mean(y), sd = sd(y)))
-abline(v=mean(y)-(1.05529*sd(y)/2)) # adds a vertical line
-
-# Comparison z-stat and effect size:
-pop_mean = y
-pop_sd = sum((y-mean(y))^2)/length(y)
-sem_x = pop_sd/sqrt(length(x))
-z_stat = (mean(x)-mean(y))/sem_x
-effect_size = (mean(x)-mean(y))/pop_sd
-# Reconstruct mean:
-mean(y)-(.35*pop_sd)
-
-# Same with n = 1
-sem_x_2 = pop_sd/sqrt(1)
-z_stat_2 = (mean(x)-mean(y))/sem_x
-effect_size_2 = (mean(x)-mean(y))/pop_sd
-
-# Check for equality of z_stat2 and effect_size_2:
-all.equal(z_stat_2,effect_size_2)
-
-
-### Caluclating n give a certain power and conf. interval
-# install.packages("pwr")
-library(pwr)
-x = c(0:10)
-y = c(0:10)+3.5
-# Calculate needed sample size for one sample t-test, given d =.5
-# a power of .8 and a sign.level of .5 == confidence int. of .95:
-pwr.t.test(d = .5, sig.level = .05, power = .8, type = "one.sample") 
-# Same for two sample:
-pwr.t.test(d = .5, sig.level = .05, power = .8, type = "two.sample") 
-# we usually assume .5 medium effect a priori
-# n = 63.76561  for each group!
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
