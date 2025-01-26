3af2d5d111655082eaba3e78d75cbd6dc4a1df76
Steffen Schwerdtfeger <88867126+StSchwerdtfeger@users.noreply.github.com>
2025-01-26 22:47:20 +0100

    Update Solution_Uebungsaufgabe.R

--- R-Basics@CIPom/Solution_Uebungsaufgabe.R
+++ R-Basics@CIPom/Solution_Uebungsaufgabe.R
@@ -1,8 +1,11 @@
-
-##################################################
-### Solution for "Übungsaufgabe.docx" and the  ###
-### examplatory data set "Uebungsaufgabe.csv": ###
-##################################################
+#######################################
+#        R-Basic Tutorial I-III       #
+#   Solution for "Übungsaufgabe.docx" #
+#       CIPom @ Charité Edition       #
+#                 by                  #
+#       Steffen Schwerdtfeger         # 
+#              12.2024                #
+#######################################
 
 # DAS SCRIPT KANN HIER VON OBEN NACH UNTEN IN EINEM ZUG KOMPLETT AUSGEFÜHRT WERDEN
 # d.h. alles markieren (ALT+SHIFT oder CMD+SHIFT), dann ALT/CMD+ENTER...
@@ -78,7 +81,6 @@ daten$fam[which(daten$fam == "n")] = "no"
 # Plot pie table of the column "fam":
 pie(table(daten$fam)) 
 
-
 # Frequencies of each option (first filter for either t1 or t2, since entries are redundant
 # in the table "daten"!!!):
 frequencies = filter(daten, time == "t1")
@@ -89,13 +91,98 @@ daten
 # ... if so, then export cleaned data set:
 write.csv(daten, "export_daten.csv")
 
+# > daten 
+#    patient_id time measurement_sysRR          fam
+# 1           1   t1               130          yes
+# 2           1   t2               122          yes
+# 3           2   t1               132           no
+# 4           2   t2               123           no
+# 5           3   t1               133 keine Angabe
+# 6           3   t2               121 keine Angabe
+# 7           4   t1               129           no
+# 8           4   t2               125           no
+# 9           5   t1               122           no
+# 10          5   t2               119           no
+# 11          6   t1               134          yes
+# 12          6   t2               127          yes
+# 13          7   t1               140 keine Angabe
+# 14          7   t2               125 keine Angabe
+# 15          8   t1               135          yes
+# 16          8   t2               128          yes
+# 17          9   t1               129          yes
+# 18          9   t2               122          yes
+# 19         10   t1               140           no
+# 20         10   t2               128           no
+# 21         11   t1               134          yes
+# 22         11   t2               122          yes
+# 23         12   t1               144          yes
+# 24         12   t2               130          yes
 
 
 
+  
+############################  
+########### BONUS EXERCISE!
+
+# Reformat the table such that the sysRR for t1 and t2 has an extra column:
+
+# Filter now cleaned data set: 
+t1 = filter(daten, time == "t1")
+t2 = filter(daten, time == "t2")
+
+# One way to do so would be cbind() each of the columns of t1 and only sysRR from t2
+# and add a empty row called RRdiff:
+RRdiff = c(0)
+sysRR_t1 = t1$measurement_sysRR
+sysRR_t2 = t2$measurement_sysRR
+fam = t1$fam
+patient_id = t1$patient_id
+reformat = as.data.frame(cbind(patient_id, sysRR_t1, sysRR_t2, fam, RRdiff))
+
+# Add sysRR difference in new column:
+for(i in 1:length(reformat[,1])){
+  reformat$RRdiff[i] = as.numeric(reformat$sysRR_t2[i]) - as.numeric(reformat$sysRR_t1[i])
+} # End for i
+
+# View results: 
+reformat
+# > reformat
+#    patient_id sysRR_t1 sysRR_t2          fam RRdiff
+# 1           1      130      122          yes     -8
+# 2           2      132      123           no     -9
+# 3           3      133      121 keine Angabe    -12
+# 4           4      129      125           no     -4
+# 5           5      122      119           no     -3
+# 6           6      134      127          yes     -7
+# 7           7      140      125 keine Angabe    -15
+# 8           8      135      128          yes     -7
+# 9           9      129      122          yes     -7
+# 10         10      140      128           no    -12
+# 11         11      134      122          yes    -12
+# 12         12      144      130          yes    -14
+
+# You want to know how many people have a RR diff over or equal to 10.
+# You can work with the abs() function for the absolut value:
+diff_10plus = reformat[abs(as.numeric(reformat$RRdiff)) >= 10,]
+# patient_id sysRR_t1 sysRR_t2          fam RRdiff
+# 3           3      133      121 keine Angabe    -12
+# 7           7      140      125 keine Angabe    -15
+# 10         10      140      128           no    -12
+# 11         11      134      122          yes    -12
+# 12         12      144      130          yes    -14
+
+length(diff_10plus[,1])
+# [1] 5
+# => five patient_ids with RR difference >=10
+
+
+
+
+#################################################################################
 #### WEITERES Beispiel für einen sogenannte "nested for loop", d.h. for loop, die
 #### eine for loop enthält:
 
-# Für die Asuwertung eiens Fragebogens ist es Usus fehlende Antworten
+# Für die Auswertung eines Fragebogens ist es Usus fehlende Antworten
 # mit dem Mittelwert aller Antworten des Probanden zu ersetzen.  
 # Antwort = irgendein ausgewählter Skalenwert für eine Frage...
 
@@ -105,6 +192,7 @@ write.csv(daten, "export_daten.csv")
 
 # WICHTIG HIER: BEISPIEL FÜR TABELLE MIT ZWEI KRITERIEN GELÖST IN EINER 
 # NESTED FOR LOOP (LOOP in einer LOOP): 
+
 
 # Data frame
 x = data.frame(cbind(c(1,NA,3),c(1,2,3), c(4,3,3)))
@@ -130,9 +218,10 @@ for(i in 1:length(x[,1])){
 # Look at result in console:
 print(x)
 
-# In Zeile 2 war ein NA und sonst nur eine 2, sodass der Mittelwert auch 2...
-# Wie man sehen kann wurde das NA mit dem entsprechendem Mittelwert der Zeile 
-# ersetzt. 
+# In Zeile 1 war ein NA, im Rest der Zeile eine 2+3, sodass der Mittelwert 
+# auch 2.5...Wie man sehen kann wurde das NA mit dem entsprechendem 
+# Mittelwert der Zeile ersetzt. 
+
 # > x
 #    X1 X2 X3
 # 1 1.0  1  4
