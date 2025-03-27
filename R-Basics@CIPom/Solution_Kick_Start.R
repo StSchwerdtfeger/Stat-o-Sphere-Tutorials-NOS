27da7dc9ba157b31c5b626cfc690b8335a0bb878
Steffen Schwerdtfeger <88867126+StSchwerdtfeger@users.noreply.github.com>
2025-03-27 15:48:14 +0100

    Update Solution_Kick_Start.R

--- R-Basics@CIPom/Solution_Kick_Start.R
+++ R-Basics@CIPom/Solution_Kick_Start.R
@@ -16,49 +16,78 @@ print("Become A Cyber!")
 ####### Recreation of table in R (a)):
 table = as.data.frame(cbind(c(1,2,3),c("Test","Test2","Test3"),c("Ey","Bee", "See")))
 
-# Alternative to keep class integrity for each column:
+# Alternative to keep class integrity for each column (another alternative: give each vector a name and
+# then bind them via data.frame():
 table = as.data.frame(c(1,2,3))
 table = cbind(table,c("Test","Test2","Test3"),c("Ey","Bee", "See"))
 
-#   V1    V2  V3
-# 1  1  Test  Ey
-# 2  2 Test2 Bee
-# 3  3 Test3 See
+# Looks nasty, since it adapted the input as col names... We could
+# rename the columns, but I leave it like it is for now:
+#  > table
+#    c(1, 2, 3) c("Test", "Test2", "Test3") c("Ey", "Bee", "See")
+#  1          1                        Test                    Ey
+#  2          2                       Test2                   Bee
+#  3          3                       Test3                   See
 
 ####### Change field first row, second column to Test1 (b)):
 table[1,2] = "Test1"
-# or, since it is a data frame object now as well:
-table$V2[1] = "Test1"
 
-#   V1    V2  V3
-# 1  1 Test1  Ey
-# 2  2 Test2 Bee
-# 3  3 Test3 See
+#  > table
+#    c(1, 2, 3) c("Test", "Test2", "Test3") c("Ey", "Bee", "See")
+#  1          1                        Test                    Ey
+#  2          2                       Test2                   Bee
+#  3          3                       Test3                   See
 
 ####### Delete first column (c)):
 table = table[,-c(1)]
 
-#      V2  V3
-# 1 Test1  Ey
-# 2 Test2 Bee
-# 3 Test3 See
+#   c("Test", "Test2", "Test3") c("Ey", "Bee", "See")
+# 1                        Test                    Ey
+# 2                       Test2                   Bee
+# 3                       Test3                   See
 
 
 ####### Decode vector (d)):
-test = c("test1", "test2", "test3")
+test = c("Test1", "Test2", "Test3")
 
 # Decode via loop (overwrites column "code"):
 for(i in 1:length(test)){
-  if(test[i] == "test1"){
+  if(test[i] == "Test1"){
     test[i] = "Something1"
   } # End if "a"
-  else if(test[i] == "test2"){
+  else if(test[i] == "Test2"){
     test[i] = "Something2"
   } # End if "b"
-  else if(test[i] == "test3"){
+  else if(test[i] == "Test3"){
     test[i] = "Something3"
   } # End if "c"
 } # End for i
+
+test
+# [1] "Something1" "Something2" "Something3"
+
+####### Decode vector (d)), alternative via targeting the above table not seperate vector:
+
+# Decode via loop (overwrites column "code"):
+for(i in 1:length(table[,1])){
+  if(table[i,1] == "Test1"){
+    table[i,1] = "Something1"
+  } # End if "a"
+  else if(table[i,1] == "Test2"){
+    table[i,1] = "Something2"
+  } # End if "b"
+  else if(table[i,1] == "Test3"){
+    table[i,1] = "Something3"
+  } # End if "c"
+} # End for i
+
+table
+# 
+# > table
+#   c("Test", "Test2", "Test3") c("Ey", "Bee", "See")
+# 1                  Something1                    Ey
+# 2                  Something2                   Bee
+# 3                  Something3                   See
 
 
 
